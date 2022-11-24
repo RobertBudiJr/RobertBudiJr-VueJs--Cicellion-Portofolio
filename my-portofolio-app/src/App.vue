825ae745087c5c6ce6c49059c4b7eb7b299ed7c8
robertbudijr <valenta_abram_29rpl@student.smktelkom-mlg.sch.id>
2022-11-24 13:39:48 +0700

    chore: make base setup

---
+++ my-portofolio-app/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HeroSection from './sections/HeroSection.vue';
+</script>
+
+<template>
+  <div class="app">
+    <HeroSection />
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ my-portofolio-app/src/sections/HeroSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="hero">
+    <div class="container">
+      <div class="left">
+        <h1>Hello! I am <span class="text-gradient-primary">Cicellion</span></h1>
+        <p class="text-lg">Front End Developer</p>
+        <div class="btn-container">
+          <button class="btn btn--primary btn--md">See What I Do</button>
+          <button class="btn btn--tertiary btn--md">Say Hello</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss"></style>
